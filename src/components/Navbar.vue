1c78ed4aa88505a0c5e029744ac5552f1dc63134
ramys00 <ravabasya06@gmail.com>
2025-09-15 14:43:24 +0800

    lots of stuff

---
+++ src/components/Navbar.vue
@@ -0,0 +1,54 @@
+<script setup></script>
+<template>
+  <nav>
+    <div class="nav-container">
+      <img
+        src="/src/assets/hourlies_logo.png"
+        alt="Hourlies"
+        style="width: 100px"
+      />
+      <div class="nav-button-container">
+        <RouterLink :to="{ name: 'home' }">
+          <p class="nav-button">Home</p>
+        </RouterLink>
+        <RouterLink :to="{ name: 'home' }">
+          <p class="nav-button">About</p>
+        </RouterLink>
+        <RouterLink :to="{ name: 'home' }">
+          <p class="nav-button">Features</p>
+        </RouterLink>
+        <RouterLink :to="{ name: 'home' }">
+          <p class="nav-button">Characters</p>
+        </RouterLink>
+        <RouterLink :to="{ name: 'home' }">
+          <p class="nav-button">Notice</p>
+        </RouterLink>
+      </div>
+    </div>
+  </nav>
+</template>
+<style scoped>
+nav {
+  position: sticky;
+  top: 0;
+  background-color: rgb(30, 29, 43);
+  padding: 15px;
+  z-index: 2;
+}
+.nav-container {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  margin-left: 20px;
+  margin-right: 20px;
+}
+.nav-button-container {
+  display: flex;
+  flex-direction: row;
+  gap: 20px;
+}
+.nav-button {
+  color: white;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,79 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import { ref, onMounted, onUnmounted } from "vue";
+import Navbar from "../components/Navbar.vue";
+
+const scrollY = ref(0);
+
+const handleScroll = () => {
+  scrollY.value = window.scrollY;
+};
+
+onMounted(() => {
+  window.addEventListener("scroll", handleScroll);
+});
+
+onUnmounted(() => {
+  window.removeEventListener("scroll", handleScroll);
+});
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="page-container">
+    <Navbar />
+    <div class="intro-container">
+      <div class="sprite-container">
+        <img src="/src/assets/sprite/az.png" alt="az" class="sprite-image" />
+        <img
+          src="/src/assets/sprite/cassie.png"
+          alt="cassie"
+          class="sprite-image"
+        />
+      </div>
+      <img
+        src="/src/assets/hourlies_logo2.png"
+        alt="Hourlies Logo"
+        class="hourlies-logo"
+      />
+    </div>
+    <div class="about-container">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam earum,
+        exercitationem dolorum quibusdam est deserunt, sequi suscipit minima
+        saepe iste a itaque nobis voluptatem quae repellendus dicta, rem sed
+        illo.
+      </p>
+    </div>
+  </div>
 </template>
+<style scoped>
+.page-container {
+  height: 1000px;
+  background-image: linear-gradient(rgb(73, 70, 104), rgb(30, 29, 43));
+}
+.intro-container {
+  display: flex;
+  flex-direction: column;
+  margin-top: 75px;
+  position: relative;
+}
+.sprite-container {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-evenly;
+}
+.about-container {
+  height: 350px;
+  background-color: rgb(30, 29, 43);
+}
+.hourlies-logo {
+  width: 350px;
+  align-self: center;
+  position: absolute;
+  top: 100px;
+  z-index: 0;
+}
+.sprite-image {
+  height: 500px;
+}
+</style>
